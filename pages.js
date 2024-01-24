b25605f6943ff6a1566ef466691f2006e667af83
parves2023 <135069947+parves2023@users.noreply.github.com>
2024-01-24 10:33:59 +0600

    asdf

---
+++ pages.js
@@ -0,0 +1,77 @@
+let pages = [
+    {
+        numb:1,
+        image:'images/1.gif',
+        name:'THE ALTERNATING HOOKS',
+        time:'30',
+        comment:'rotate your shoulder and hips  ',
+    },
+    {
+        numb:2,
+        image:'images/2.gif',
+        name:'DUMBBELL KICKBACKS',
+        time:'30',
+        comment:'ex-hale as you pullup the dumble and in-hale as you lower them',
+        // sets:'15',
+    },
+    {
+        numb:3,
+        image:'images/3.gif',
+        name:'DUMBBELL MILITARY PRESS.',
+        time:'30',
+        comment:'.you should fill your arms are burning',
+        // sets:'15',
+
+    },
+    {
+        numb:4,
+        image:'images/4.gif',
+        name:'DUMBBELL REVERSE CURL',
+        time:'30',
+        comment:'this exercise increase the strength of your shoulder',
+        // sets:'15',
+    },
+    {
+        numb:5,
+        image:'images/5.gif',
+        name:'DUMBBELL FRONT RAISE',
+        time:'30',
+        comment:'pull your shoulders back',
+        // sets:'15',
+    },
+    {
+        numb:6,
+        image:'images/6.gif',
+        name:'DUMBBELL SIDE LATERAL RAISE',
+        time:'30',
+        comment:'aaaaeeee please pull your shoulders back',
+        // sets:'15',
+
+    },
+    {
+        numb:7,
+        image:'images/7.gif',
+        name:'ALTERNATE HAMMER CURL',
+        time:'30',
+        comment:'squiz your bicep at the top',
+        // sets:'15',
+
+    },
+    {
+        numb:8,
+        image:'images/8.gif',
+        name:'DUMBBELL TRICEPS EXTENSION',
+        time:'30',
+        comment:'please squiz your tryceps up while raising your hands up',
+        // sets:'15',
+
+    },
+    {
+        numb:9,
+        image:'images/9.gif',
+        name:'STANDING DUMBBELL CURL',
+        time:'30',
+        comment:'squiz your bicep at the top',
+         // sets:'15',
+    },
+    ];
