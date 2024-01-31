9987e9e7ede1b705762d1a0d4d94fc1769c9d2af
parves2023 <135069947+parves2023@users.noreply.github.com>
2024-02-01 00:24:42 +0600

    good desing

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
+let theimages = document.getElementById('theimages');
 let time = 30;
 let timerInterval;
+let isPaused = false;
+let exerciseName = document.getElementById('exerciseName');
+let nextBtn = document.getElementById('theNextButton');
+let PreviousButton = document.getElementById('thePreviousButton');
+let progress = 0;
+let pagecount = 0 ;
+const maxProgress = pages.length;
+const progressBar = document.getElementById('progress-bar');
+const progressText = document.getElementById('progress-text');
+progressText.innerHTML = maxProgress;
+// console.log(pages[pagecount].hasOwnProperty('sets'));
 
 function updateTimerDisplay() {
   const minutes = Math.floor(time / 60);
   const seconds = time % 60;
   document.getElementById('timer').innerText = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+
+  if (time === 18) {
+    if(pages[pagecount].hasOwnProperty('sets')){
+      clearInterval(timerInterval);
+      isPaused = true;
+      document.getElementById('pauseButton').innerText = 'RESUME';
+      clearInterval(timerInterval);
+      speak(`${pages[pagecount].comment} & do it until ${pages[pagecount].sets} times`);
+    }else{
+      speak(`${pages[pagecount].comment}`);
+    }
+    
+  }
+
+  if (time <= 3 && time > 0) {
+    speak(time.toString());
+  }
 }
+
 
 function startTimer() {
   timerInterval = setInterval(function () {
@@ -13,17 +43,117 @@ function startTimer() {
     updateTimerDisplay();
     if (time === 0) {
       clearInterval(timerInterval);
+      const audio = new Audio('audio/bell audio.m4a'); // Replace 'audio1.mp3' with your audio file path
+      audio.play();
+      nextBtn.onclick();
+      time = parseInt(`${pages[pagecount].time}`);
     }
   }, 1000);
 }
 
-function pauseTimer() {
-  clearInterval(timerInterval);
+
+function togglePause() {
+  if (time > 0) {
+    if (isPaused) {
+      isPaused = false;
+      document.getElementById('pauseButton').innerText = 'PAUSE';
+      startTimer();
+    } else {
+      isPaused = true;
+      document.getElementById('pauseButton').innerText = 'RESUME';
+      clearInterval(timerInterval);
+    }
+  }
+};
+
+function showSecondSection() {
+document.getElementById('firstSection').style.display = 'none';
+document.getElementById('secondSection').style.visibility = 'visible';
+speak(`start. 30 second. ${pages[pagecount].name}`);
+startTimer();
 }
 
-document.getElementById('pauseButton').addEventListener('click', function () {
-  pauseTimer();
-});
+function speak(text) {
+  const speechSynthesis = window.speechSynthesis;
+  const speechUtterance = new SpeechSynthesisUtterance(text);
+  speechSynthesis.speak(speechUtterance);
+};
 
-// Start the timer when the page loads
-startTimer();
+
+nextBtn.onclick = ()=> {
+  if(pagecount < pages.length - 1){
+    clearInterval(timerInterval);
+    pagecount++;
+    theimages.src = pages[pagecount].image;
+    exerciseName.innerHTML = pages[pagecount].name;
+    time = pages[pagecount].time;
+    isPaused = false;
+    document.getElementById('pauseButton').innerText = 'PAUSE';
+    startTimer();
+    speak(`THE NEXT. ${pages[pagecount].name}`);
+    const audio = new Audio('audio/bell audio.m4a');
+      audio.play();
+      increaseProgress();
+  }else{
+      clearInterval(timerInterval);
+      pagecount = 0 ;
+      progress = 0;
+      time = 30;
+      theimages.src = pages[pagecount].image;
+      exerciseName.innerHTML = pages[pagecount].name;
+      time = pages[pagecount].time;
+      startTimer();
+      updateProgressBar();
+      speak(`THE NEXT. ${pages[pagecount].name}`);
+    const audio = new Audio('audio/bell audio.m4a');
+      audio.play();
+  }
+}
+
+
+PreviousButton.onclick = ()=> {
+if(pagecount > 0){
+    pagecount--;
+    theimages.src = pages[pagecount].image;
+    exerciseName.innerHTML = pages[pagecount].name;
+    clearInterval(timerInterval);
+    time = pages[pagecount].time;
+    startTimer();
+    speak(`THE NEXT. ${pages[pagecount].name}`);
+    const audio = new Audio('audio/bell audio.m4a');
+      audio.play();
+      decreaseProgress();
+    }else{
+      pagecount = pages.length;
+      progress = pages.length;
+      updateProgressBar();
+      theimages.src = pages[pagecount].image;
+      exerciseName.innerHTML = pages[pagecount].name;
+      speak(`THE NEXT. ${pages[pagecount].name}`);
+    const audio = new Audio('audio/bell audio.m4a');
+      audio.play(); 
+    }
+}
+
+
+function increaseProgress() {
+  if (progress < maxProgress) {
+    progress++;
+    updateProgressBar();
+  }
+}
+
+
+function decreaseProgress(){
+if(progress > 0){
+  progress--;
+  updateProgressBar();
+  }
+}
+
+
+function updateProgressBar() {
+  const percentage = (progress / maxProgress) * 100;
+  progressBar.style.width = percentage + '%';
+  progressText.textContent = `${progress}/${maxProgress}`;
+}
